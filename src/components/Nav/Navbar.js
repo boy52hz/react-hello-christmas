d89dbe4fc18dcf5efc63b0150a8536eb9aa7eadd
boy52hz <ggwp.zero@gmail.com>
2020-11-10 15:31:47 +0700

    feat: navbar background when scrolling down

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -2,10 +2,41 @@ import React from 'react';
 import NavItem from './NavItem';
 import './Nav.css';
 
-class Navbar extends React.Component {
+class Navbar extends React.Component { 
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrolled: false
+    }
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  handleScroll() {
+    let scrollY = window.scrollY;
+    if (scrollY > 200) {
+      this.setState({ scrolled: true });
+    } else {
+      this.setState({ scrolled: false })
+    }
+  }
+  
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentDidUpdate() {
+    if (this.state.scrolled !== this.state.scrolled) {
+      this.handleScroll()
+    }
+  }
+
   render() {
+    let navClass = ['Navbar'];
+    if (this.state.scrolled) {
+      navClass.push('scrolled');
+    }
     return (
-      <nav className="Navbar">
+      <nav className={ navClass.join(" ") }>
         <h2 id="logo">Logo</h2>
         <ul>
           <NavItem name="Home" to="#Hero"/>
--- src/components/Wish/Wish.js
+++ src/components/Wish/Wish.js
@@ -6,10 +6,10 @@ import Button from '../Button/Button';
 class Wish extends React.Component {
   render() {
     return (
-      <section id="Wish" class="Wish">
+      <section id="Wish" className="Wish">
         <div className="Wish__container">
 					<h1>Wish</h1>
-					<div class="Wish__main">
+					<div className="Wish__main">
 						<img src="https://source.unsplash.com/random/500x400" alt="christmas sock"/>
 					</div>
           <div className="Wish__btn-group">
