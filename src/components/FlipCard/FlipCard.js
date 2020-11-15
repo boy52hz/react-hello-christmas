1f5c8de87609ff80b782a958c6ff1823fab17dff
Jzaza007 <jing182544@gmail.com>
2020-11-15 15:27:03 +0700

    Fixed FlipCard bug and History__gallery justify

--- src/components/FlipCard/FlipCard.js
+++ src/components/FlipCard/FlipCard.js
@@ -11,13 +11,14 @@ class FlipCard extends Component {
 		this.handleClick = this.handleClick.bind(this);
 	}
 	handleClick(e) {
-		this.setState(oldState => ({ isFlipped: !oldState.isFlipped }))
+		e.preventDefault();
+		this.setState(oldState => ({ isFlipped: !oldState.isFlipped }));
 	}
 	render() {
 		return (
 			<ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
-        <img src={ this.props.imgFront } alt="Front img" onClick={ this.handleClick }/>
-				<img src={ this.props.imgBack } alt="Back img" onClick={ this.handleClick }/>
+        <img src={ this.props.imgFront } alt="Front img" onClick={ this.handleClick } style={ this.state.isFlipped ? { visibility: "hidden" }: null }/>
+				<img src={ this.props.imgBack } alt="Back img" onClick={ this.handleClick } style={ !this.state.isFlipped ? { visibility: "hidden" }: null }/>
       </ReactCardFlip>
 		)
 	}
