b3f86a4b6e43798ed410ce292572a41b49927072
spyy18 <spyyer18@gmail.com>
2020-11-15 20:59:16 +0700

    feat: styled footer and added props img

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,8 +7,19 @@ import Poi from '../../static/img/about-us/poi.png';
 import J from '../../static/img/about-us/j.png';
 import Ben from '../../static/img/about-us/ben.png';
 import Bell from '../../static/img/about-us/cutbell.png';
-import Tree from '../../static/img/about-us/tree.png';
+import BigTree from '../../static/img/about-us/tree+light.gif';
+import SmallTree from '../../static/img/about-us/tree+Light+2.gif';
+import SmallTreeRight from '../../static/img/about-us/treeright.png';
+import SmallTreeCenterRight from '../../static/img/about-us/tree-centerright.png';
+import SmallTreeCut from '../../static/img/about-us/small-tree-cut-left.png';
+import SmallTreeRightCut from '../../static/img/about-us/tree-right-cut.png';
+import SmallBellLeft from '../../static/img/about-us/bellleft.png';
+import SmallBellRight from '../../static/img/about-us/bellright.png';
+import SmallBellRightCut from '../../static/img/about-us/bellrightcut.png';
+
+
 import Logo from '../../static/img/about-us/logo.png';
+
 
 
 
@@ -20,15 +31,48 @@ class Footer extends React.Component {
 			<section id="Footer">
 				<img class="big-bell" src={ Bell } alt="big bell"/>	
 				<ul>
-					<li><img src={ Yok } alt="Yok"/><p>YOK</p></li>
-					<li><img src={ Prew } alt="Prew"/><p>PREW</p></li>
-					<li><img src={ Spy } alt="Spy"/><p>SPY</p></li>
-					<li><img src={ Poi } alt="Poi"/><p>POI</p></li>
-					<li><img src={ Ben } alt="Ben"/><p>BEN</p></li>
-					<li><img src={ J } alt="J"/><p>J</p></li>
+					<li>
+						<img src={ Yok } alt="Yok"/>
+						<p class="name">YOK</p>
+						<p class="work">Web Design</p>
+					</li>
+					<li>
+						<img src={ Prew } alt="Prew"/>
+						<p class="name">PREW</p>
+						<p class="work">Web Design</p>
+					</li>
+					<li>
+						<img src={ Spy } alt="Spy"/>
+						<p class="name">SPY</p>
+						<p class="work">Front End</p>
+					</li>
+					<li>
+						<img src={ Poi } alt="Poi"/>
+						<p class="name">POI</p>
+						<p class="work">Front End</p>
+					</li>
+					<li>
+						<img src={ Ben } alt="Ben"/>
+						<p class="name">BEN</p>
+						<p class="work">Front End</p>
+					</li>
+					<li>
+						<img src={ J } alt="J"/>
+						<p class="name">J</p>
+						<p class="work">Infrastructure</p>
+					</li>
 				</ul>
-				<img class="big-tree" src={ Tree } alt="big tree"/>
-				<img src={ Logo } alt="logo"/>
+				<img class="big-tree" src={ BigTree } alt="big tree"/>
+				<img class="small-tree" src={ SmallTree } alt="small tree"/>
+				<img class="small-tree-right" src={ SmallTreeRight } alt="small tree right"/>
+				<img class="small-tree-centerright" src={ SmallTreeCenterRight } alt="small tree center right"/>
+				<img class="small-tree-cut" src={ SmallTreeCut } alt="small tree left"/>
+				<img class="small-tree-right-cut" src={ SmallTreeRightCut } alt="small tree right cut"/>
+				<img class="small-bell-left" src={ SmallBellLeft } alt="small bell left"/>
+				<img class="small-bell-right" src={ SmallBellRight } alt="small bell right"/>
+				<img class="small-bell-left2" src={ SmallBellLeft } alt="small bell left2"/>
+				<img class="small-bell-right-cut" src={ SmallBellRightCut } alt="small bell right cut"/>
+				<img class="logo-hello"src={ Logo } alt="logo"/>
 			</section>
 		);
 	}
