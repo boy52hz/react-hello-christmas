ba79a6c8e3b012c8c3859a5e2388fff4a45c8cb8
boy52hz <ggwp.zero@gmail.com>
2020-11-16 17:03:45 +0700

    added: Waterdrop props and some keyframe

--- src/components/Countdown/Countdown.js
+++ src/components/Countdown/Countdown.js
@@ -3,6 +3,7 @@ import './Countdown.scss';
 
 import CandyCane from '../../static/img/candycane.png';
 import LigthWire from '../../static/img/lightwire.png';
+import WaterDrop from '../../static/img/waterdrop.png';
 
 const timerGlow = [
 	{ boxShadow: "0px 0px 5px 8px rgba(224, 53, 187, 0.8)" },
@@ -19,55 +20,61 @@ class Countdown extends React.Component {
 		seconds: 0
 	}
 
-  componentDidMount() {
-    this.tick = setInterval(() => {
-		const xmas = new Date(2020, 11, 25).getTime();
-		const now = new Date().getTime();
-		const distance = xmas - now;
-		const days = Math.floor(distance / (1000 * 60 * 60 * 24)).toString().padStart(2, 0);
-  		const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, 0);
-  		const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, 0);
-  		const seconds = Math.floor((distance % (1000 * 60)) / 1000).toString().padStart(2, 0);
-      this.setState({ days, hours, minutes, seconds });
-    }, 1000);
-  }
+	componentDidMount() {
+		this.tick = setInterval(() => {
+			const xmas = new Date(2020, 11, 25).getTime();
+			const now = new Date().getTime();
+			const distance = xmas - now;
+			const days = Math.floor(distance / (1000 * 60 * 60 * 24)).toString().padStart(2, 0);
+			const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, 0);
+			const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, 0);
+			const seconds = Math.floor((distance % (1000 * 60)) / 1000).toString().padStart(2, 0);
+			this.setState({ days, hours, minutes, seconds });
+		}, 1000);
+	}
 
-  componentWillUnmount() {
-    if (this.tick) {
-      clearInterval(this.tick);
-    }
-  }
+	componentWillUnmount() {
+		if (this.tick) {
+			clearInterval(this.tick);
+		}
+	}
 
-  render() {
+	render() {
 		const state = this.state;
-    return (
-      <section id="Countdown">
-        <div className="Countdown__container">
+		return (
+			<section id="Countdown">
+				<div className="waterdrop">
+					<img src={ WaterDrop } alt="Waterdrop 1"/>
+					<img src={ WaterDrop } alt="Waterdrop 2"/>
+					<img src={ WaterDrop } alt="Waterdrop 3"/>
+				</div>
+				<div className="Countdown__container">
 					<h1>Countdown</h1>
-          <div className="Countdown__box">
-						<img id="candycane" src={ CandyCane } alt="Candy Cane"/>
-						<img id="lightwire" src={ LigthWire } alt="Light Wire"/>
+
+					<div className="Countdown__box">
+						<img id="candycane" src={CandyCane} alt="Candy Cane" />
+						<img id="lightwire" src={LigthWire} alt="Light Wire" />
 						<div className="Countdown__item">
 							<div className="title">Days</div>
-							<div className="timer" style={ timerGlow[0] }>{ state.days }</div>
+							<div className="timer" style={timerGlow[0]}>{state.days}</div>
 						</div>
 						<div className="Countdown__item">
 							<div className="title">Hours</div>
-							<div className="timer" style={ timerGlow[1] }>{ state.hours }</div>
+							<div className="timer" style={timerGlow[1]}>{state.hours}</div>
 						</div>
 						<div className="Countdown__item">
 							<div className="title">Minutes</div>
-							<div className="timer" style={ timerGlow[2] }>{ state.minutes }</div>
+							<div className="timer" style={timerGlow[2]}>{state.minutes}</div>
 						</div>
 						<div className="Countdown__item">
 							<div className="title">Seconds</div>
-							<div className="timer" style={ timerGlow[3] }>{ state.seconds }</div>
+							<div className="timer" style={timerGlow[3]}>{state.seconds}</div>
 						</div>
-          </div>
-        </div>
-      </section>
-    );
-  }
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default Countdown;
